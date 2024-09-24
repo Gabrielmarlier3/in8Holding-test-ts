39e178995d23a7b1f690819674839b832d9de995
gabrielmarlier3 <gabrielmarliere2005@gmail.com>
2024-09-24 14:20:33 -0300

    Fix: Troquei o nome para resolver um erro

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { checkEnvVariables } from "./config/envConfig";
 import cron from "node-cron";
 import { fetchData, processData } from "./services/WebScrapingService";
-import notebookRoutes from './routes/NotebookRoutes';
+import notebookRoutes from './routes/notebookRoutes';
 import { checkDatabaseConnection } from "./config/database";
 
 
